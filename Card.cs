0813f4cd169cff898f9f8977191038313776e61e
MarcusReyes619 <129239355+MarcusReyes619@users.noreply.github.com>
2025-04-29 19:19:13 -0600

    Add project files.

---
+++ Card.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoingATHING
+{
+    enum Rarity
+    {
+        COMMON,
+        RARE,
+        IDONTCAREATTHISPOINT
+    }
+    internal class Card
+    {
+        string name;
+        Rarity rarity;
+        int number;
+        string discription;
+
+        public Card(string name, Rarity rarity, int number, string discription)
+        {
+            this.name = name;
+            this.rarity = rarity;
+            this.number = number;
+            this.discription = discription;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DoingATHING
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Name: ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine(": ");
+        }
+    }
+}
